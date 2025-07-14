87043c8b38d8bfb12223bb353c2f0458157b3f0e
Ebose Esekheilu <ebose.esekheilu@ucalgary.ca>
2025-07-13 20:26:22 -0600

    Revert "Delete trafficlight.h"
    This reverts commit 3adc34722bedff3f6ff35beca2bc0157d13d5ec9.

---
+++ trafficlight.h
@@ -0,0 +1,20 @@
+//
+// Created by Ebose on 2025-07-07.
+//
+
+#ifndef TRAFFICLIGHT_H
+#define TRAFFICLIGHT_H
+
+class trafficlight {
+private:
+    std::string direction; //N,S,E or W
+    int timer;
+    enum state {Red, Green, Yellow};
+public:
+    trafficlight(std::string dir);
+    void changecolour();
+};
+
+
+
+#endif //TRAFFICLIGHT_H
